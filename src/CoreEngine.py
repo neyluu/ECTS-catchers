c07b28e89441221ce2f9e520962e1391df1184a5
neyluu <75064225+neyluu@users.noreply.github.com>
2025-03-26 13:42:58 +0100

    Settings file and game window initialization

--- src/CoreEngine.py
+++ src/CoreEngine.py
@@ -1,12 +1,24 @@
+import sys
 import pygame as pg
+
+from src.common import Settings
+
+pg.init()
+pg.display.set_caption(Settings.TITLE)
+pg.display.set_mode((Settings.SCREEN_WIDTH, Settings.SCREEN_HEIGHT))
 
 class CoreEngine():
 
     def __init__(self):
         self.running = True
+        self.screen = pg.display.get_surface()
+        self.clock = pg.time.Clock()
 
     def handleEvent(self):
-        pass
+        for event in pg.event.get():
+
+            if event.type == pg.QUIT:
+                self.running = False
 
     def update(self):
         pass
@@ -16,4 +28,7 @@ class CoreEngine():
 
     def run(self):
         while self.running:
-            pass
+            self.handleEvent()
+            self.update()
+            self.draw()
+            self.clock.tick(Settings.TARGET_FPS)
