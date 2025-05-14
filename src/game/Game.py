563cb1c3ed75ee088aa8f46127f13ae1f0475eba
neyluu <75064225+neyluu@users.noreply.github.com>
2025-05-14 14:58:27 +0200

    Added change level animation

--- src/game/Game.py
+++ src/game/Game.py
@@ -1,3 +1,4 @@
+import math
 import pygame as pg
 
 from src.game.Player import Player
@@ -21,24 +22,47 @@ class Game:
 
         self.addPlayerDataToLevels()
 
+        self.dt = 0
+
+        self.nextLevel = 0
+        self.levelChangeTimer : float = 0
+        self.levelChangeDelay : float = 1 # seconds
+        self.isLevelChanging : bool = False
+
+        self.blinkOpacity = 0
+
 
     def handleEvent(self, event):
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_5:
                 self.currentLevel = 0 if self.currentLevel - 1 < 0 else self.currentLevel - 1
-            if event.key == pg.K_6:
-                self.currentLevel = len(self.levels) - 1 if self.currentLevel + 1 > len(self.levels) - 1 else self.currentLevel + 1
+
+            # DEBUG level changing
+            # if event.key == pg.K_6:
+            #     self.currentLevel = len(self.levels) - 1 if self.currentLevel + 1 > len(self.levels) - 1 else self.currentLevel + 1
 
         self.levels[self.currentLevel].handleEvents(event)
         self.player.handleEvent(event)
 
 
     def update(self, dt : float):
+        self.dt = dt
+
         self.levels[self.currentLevel].update(dt)
         self.player.update(dt)
 
+        if self.isLevelChanging:
+            self.levelChangeTimer += dt
+            if self.levelChangeTimer > self.levelChangeDelay:
+                self.handleLevelChange()
+
+
         if self.player.playerData.currentLevel < len(self.levels) and self.currentLevel != self.player.playerData.currentLevel:
-            self.handleLevelChange()
+            self.isLevelChanging = True
+            self.nextLevel = self.player.playerData.currentLevel
+        if self.player.playerData.currentLevel >= len(self.levels):
+            # print("Game over!")
+            pass
 
 
     def draw(self, screen : pg.Surface):
@@ -46,18 +70,44 @@ class Game:
         self.levels[self.currentLevel].draw(screen)
         self.player.draw(screen)
 
+        if self.isLevelChanging:
+            self.levelChangeBlink(screen)
+
 
     def setBackgroundColor(self, color : pg.Color):
         self.backgroundColor = color
 
 
     def handleLevelChange(self):
-        print(f"Changing level to {self.player.playerData.currentLevel}")
-        self.currentLevel = self.player.playerData.currentLevel
+        print(f"Changing level to {self.nextLevel}")
+        self.currentLevel = self.nextLevel
         self.levels[self.currentLevel].reset()
         self.player.tileMap = self.levels[self.currentLevel].map
+
+        self.levelChangeTimer = 0
+        self.isLevelChanging = False
 
 
     def addPlayerDataToLevels(self):
         for level in self.levels:
             level.playerData = self.player.playerData
+
+
+    def levelChangeBlink(self, screen : pg.Surface):
+        time = (self.levelChangeTimer % self.levelChangeDelay) / self.levelChangeDelay
+        self.blinkOpacity = 255 * math.sin(math.pi * time)
+
+        # if self.levelChangeTimer < self.levelChangeDelay / 2:
+        #     self.blinkOpacity += self.levelChangeDelay / self.dt
+        #     if self.blinkOpacity > 255:
+        #         self.blinkOpacity = 255
+        # else:
+        #     self.blinkOpacity -= self.levelChangeDelay / self.dt
+        #     if self.blinkOpacity < 0:
+        #         self.blinkOpacity = 0
+
+        print(f"Opacity: {self.blinkOpacity}")
+
+        overlay = pg.Surface((self.canvas.width, self.canvas.height), pg.SRCALPHA)
+        overlay.fill((0, 0, 0, self.blinkOpacity))
+        screen.blit(overlay, (self.canvas.left, self.canvas.top))
