bae85f2076da38d0d1ecd51b62bf37571d1525cc
kacekhehee <kjanoska5@gmail.com>
2025-05-28 12:53:56 +0200

    Added CoinCounter logic with Icon

--- src/gui/InGameUI.py
+++ src/gui/InGameUI.py
@@ -1,7 +1,8 @@
-# src/gui/InGameUI.py
 import pygame as pg
 from src.gui.Timer import Timer
 from src.gui.HealthBar import HealthBar
+from src.gui.CoinCounter import CoinCounter
+
 
 class InGameUI:
     def __init__(self, canvas: pg.Rect, playerData):
@@ -9,7 +10,7 @@ class InGameUI:
         self.playerData = playerData
 
         self.gameTimer = Timer()
-        self.timerPosition = (self.canvas.x + self.canvas.width -450, self.canvas.y + 2)
+        self.timerPosition = (self.canvas.x + self.canvas.width - 450, self.canvas.y + 2)
 
         self.healthBar = HealthBar(
             playerData=self.playerData,
@@ -20,15 +21,23 @@ class InGameUI:
             displayHeartHeight=60,
             heartPadding=6
         )
-        self.healthBarPosition = (self.canvas.x +120, self.canvas.y + 10)
+        self.healthBarPosition = (self.canvas.x + 120, self.canvas.y + 10)
+        self.coinCounter = CoinCounter(
+            playerData=self.playerData,
+            maxPoints=30,
+            fontSize=60
+        )
+        self.coinCounterPosition = (self.canvas.x + 700, self.canvas.y + 10)
 
     def update(self):
         self.gameTimer.update()
         self.healthBar.update()
+        self.coinCounter.update()
 
     def draw(self, screen: pg.Surface):
         self.gameTimer.draw(screen, self.timerPosition)
         self.healthBar.draw(screen, self.healthBarPosition)
+        self.coinCounter.draw(screen, self.coinCounterPosition)
 
     def pauseTimer(self):
         self.gameTimer.pause()
