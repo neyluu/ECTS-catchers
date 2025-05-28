6a9acc02ccb76e2db48da4c6340ad6391b077796
neyluu <75064225+neyluu@users.noreply.github.com>
2025-05-28 18:01:56 +0200

    Added PauseMenu background

--- src/scenes/GameScene.py
+++ src/scenes/GameScene.py
@@ -1,6 +1,7 @@
 import pygame as pg
 
 from src.config import Settings
+from src.gui.PauseMenu import PauseMenu
 from src.scenes.Scene import Scene
 from src.game.Game import Game
 from src.game.PlayerKeymap import PlayerKeymap
@@ -19,6 +20,9 @@ class GameScene(Scene):
         self.leftGame.player.color = pg.Color(255, 255, 0, 0)
         self.rightGame.player.color = pg.Color(0, 255, 255, 0)
 
+        self.paused : bool = False
+        self.pauseMenu = PauseMenu()
+
 
     def handleEvent(self, event):
         if event.type == pg.KEYDOWN:
@@ -26,14 +30,26 @@ class GameScene(Scene):
                 self.sceneManager.setCurrentScene(2)
             elif event.key == pg.K_1:
                 self.sceneManager.setCurrentScene(0)
+            if event.key == Settings.KEYMAP_PAUSE:
+                self.paused = not self.paused
+                if self.paused:
+                    self.onPause()
+                else:
+                    self.onUnPause()
 
         self.leftGame.handleEvent(event)
         self.rightGame.handleEvent(event)
+
+        if self.paused:
+            self.pauseMenu.handleEvent(event)
 
 
     def update(self, dt : float):
         self.leftGame.update(dt)
         self.rightGame.update(dt)
+
+        if self.paused:
+            self.pauseMenu.update(dt)
 
 
     def draw(self, screen : pg.Surface):
@@ -41,3 +57,15 @@ class GameScene(Scene):
 
         self.leftGame.draw(screen)
         self.rightGame.draw(screen)
+
+        if self.paused:
+            self.pauseMenu.draw(screen)
+
+
+    def onPause(self):
+        pass
+
+
+    def onUnPause(self):
+        pass
+
