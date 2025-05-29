3544ba6c41202e808f4afa98effbf969398db3c8
neyluu <75064225+neyluu@users.noreply.github.com>
2025-05-29 20:54:32 +0200

    Fixed Block textures loading

--- src/game/map/tiles/Block.py
+++ src/game/map/tiles/Block.py
@@ -1,11 +1,13 @@
 from src.game.map.tiles.Tile import Tile
 
-import pygame as pg
-
 
 class Block(Tile):
-    def __init__(self):
+    def __init__(self, path : str):
         super().__init__()
         self.isCollision = True
-        self.path = "assets/textures/blocks/grey_brick.png"
-        self.loadTexture()
+
+        if path == '':
+            path = "assets/textures/blocks/grey_brick_1.png"
+            print("ERROR: block texture path is empty!")
+
+        self.loadTexture(path)
