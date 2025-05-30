84348aac44ecd3d2c3657e88361fa386d523b529
neyluu <75064225+neyluu@users.noreply.github.com>
2025-05-30 21:25:49 +0200

    Fixed lava taking to much hp when walking in it

--- src/game/map/tiles/Lava.py
+++ src/game/map/tiles/Lava.py
@@ -1,35 +1,44 @@
+import pygame as pg
+
 from src.game.PlayerData import PlayerData
 from src.game.map.tiles.Trigger import Trigger
 import src.config.PowerUpsConfig as config
 
 
 class Lava(Trigger):
+    inLava : bool = False
+    timer : float = 0
+
+
     def __init__(self):
         super().__init__()
 
         self.color = "firebrick"
-
         self.damageDelay : int = config.LAVA_DAMAGE_DELAY_TIME # seconds
-        self.timer : float = 0
-
         self.playerData : PlayerData = None
 
 
     def update(self, dt: float):
         if not self.isActive and self.playerData is not None:
-            self.timer += dt
-            if self.timer > self.damageDelay:
+            now = pg.time.get_ticks() / 1000
+            if now - Lava.timer > self.damageDelay:
                 self.playerData.gotDamaged = True
-                self.timer = 0
+                Lava.timer = now
 
 
     def reset(self):
         super().reset()
+        Lava.inLava = False
 
 
     def onTrigger(self, playerData : PlayerData):
         if self.wasEntered():
             return
 
+
+        Lava.inLava = True
         self.playerData = playerData
-        self.playerData.hp -= 1
+
+        if not Lava.inLava:
+            Lava.timer = pg.time.get_ticks() / 1000
+            self.playerData.hp -= 1
