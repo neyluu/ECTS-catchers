d7588f170a9c5627b2a3cea7c332953201d21cc7
kacekhehee <kjanoska5@gmail.com>
2025-06-04 12:36:29 +0200

    Added StartMenu with the buttons

---
+++ src/gui/Button.py
@@ -0,0 +1,122 @@
+# src/gui/Button.py
+import pygame as pg
+
+
+class Button:
+    def __init__(self, x, y, width, height, texturePath,
+                 text='Button', rotationAngle=0,
+                 fontPath=None, fontSize=30, textColor=(255, 255, 255),
+                 outlineColor=(0, 0, 0), outlineThickness=2,
+                 hoverEffectColor=(255, 255, 255, 50),
+                 action=None):
+
+        self.xPosition = x
+        self.yPosition = y
+        self.buttonWidth = width
+        self.buttonHeight = height
+        self.texturePath = texturePath
+        self.text = text
+        self.rotationAngle = rotationAngle
+        self.fontPath = fontPath
+        self.fontSize = fontSize
+        self.textColor = textColor
+        self.outlineColor = outlineColor
+        self.outlineThickness = outlineThickness
+        self.hoverEffectColor = hoverEffectColor
+        self.action = action
+
+        self.isHovered = False
+
+        if not pg.font.get_init():
+            pg.font.init()
+
+        if self.fontPath:
+            self.font = pg.font.Font(self.fontPath, self.fontSize)
+        else:
+            self.font = pg.font.Font(None, self.fontSize)
+
+        originalBaseImage = pg.image.load(self.texturePath).convert_alpha()
+        self.baseScaledImage = pg.transform.scale(originalBaseImage, (self.buttonWidth, self.buttonHeight))
+
+        self.texture = None
+        self.rect = pg.Rect(x, y, 0, 0)
+        self.mask = None
+
+        self._rebuildTexture()
+
+        if self.hoverEffectColor and len(self.hoverEffectColor) == 4 and self.hoverEffectColor[3] > 0:
+            self.hoverSurface = pg.Surface(self.rect.size, pg.SRCALPHA)
+            self.hoverSurface.fill(self.hoverEffectColor)
+        else:
+            self.hoverSurface = None
+
+    def _rebuildTexture(self):
+        imageToTransform = self.baseScaledImage.copy()
+
+        if self.text and self.font:
+            mainTextSurf = self.font.render(self.text, True, self.textColor)
+            textRenderRect = mainTextSurf.get_rect(center=imageToTransform.get_rect().center)
+
+            if self.outlineThickness > 0 and self.outlineColor:
+                for dxOutline in range(-self.outlineThickness, self.outlineThickness + 1):
+                    for dyOutline in range(-self.outlineThickness, self.outlineThickness + 1):
+                        if dxOutline == 0 and dyOutline == 0:
+                            continue
+                        outlineSurf = self.font.render(self.text, True, self.outlineColor)
+                        imageToTransform.blit(outlineSurf, (textRenderRect.x + dxOutline, textRenderRect.y + dyOutline))
+
+            imageToTransform.blit(mainTextSurf, textRenderRect)
+
+        self.texture = pg.transform.rotate(imageToTransform, self.rotationAngle)
+
+        originalCenterX = self.xPosition + self.buttonWidth / 2
+        originalCenterY = self.yPosition + self.buttonHeight / 2
+        self.rect = self.texture.get_rect(center=(originalCenterX, originalCenterY))
+
+        self.mask = pg.mask.from_surface(self.texture)
+
+    def _checkCollision(self, pos):
+        if self.rect.collidepoint(pos):
+            local_x = pos[0] - self.rect.x
+            local_y = pos[1] - self.rect.y
+            if 0 <= local_x < self.mask.get_size()[0] and \
+                    0 <= local_y < self.mask.get_size()[1]:
+                if self.mask.get_at((local_x, local_y)):
+                    return True
+        return False
+
+    def draw(self, screen: pg.Surface):
+        screen.blit(self.texture, self.rect.topleft)
+
+        if self.isHovered and self.hoverSurface:
+            screen.blit(self.hoverSurface, self.rect.topleft)
+
+    def handleEvent(self, event: pg.event.Event):
+        if event.type == pg.MOUSEMOTION:
+            self.isHovered = self._checkCollision(event.pos)
+        elif event.type == pg.MOUSEBUTTONDOWN:
+            if event.button == 1 and self._checkCollision(event.pos):
+                if self.action:
+                    self.action()
+                return True
+        return False
+
+    def updateText(self, newText):
+        if self.text != newText:
+            self.text = newText
+            self._rebuildTexture()
+            if self.hoverSurface and self.hoverEffectColor and len(self.hoverEffectColor) == 4 and \
+                    self.hoverEffectColor[3] > 0:
+                if self.hoverSurface.get_size() != self.rect.size:
+                    self.hoverSurface = pg.Surface(self.rect.size, pg.SRCALPHA)
+                    self.hoverSurface.fill(self.hoverEffectColor)
+
+    def setRotationAngle(self, newRotationAngle):
+        if self.rotationAngle != newRotationAngle:
+            self.rotationAngle = newRotationAngle
+            self._rebuildTexture()
+            if self.hoverSurface and self.hoverEffectColor and len(self.hoverEffectColor) == 4 and \
+                    self.hoverEffectColor[3] > 0:
+                if self.hoverSurface.get_size() != self.rect.size:
+                    self.hoverSurface = pg.Surface(self.rect.size, pg.SRCALPHA)
+                    self.hoverSurface.fill(self.hoverEffectColor)
--- src/scenes/MainMenu.py
+++ src/scenes/MainMenu.py
@@ -1,23 +1,153 @@
+# src/scenes/main_menu.py
 import pygame as pg
+import sys
 
 from src.scenes.Scene import Scene
-
+from src.gui.Button import Button
 
 class MainMenu(Scene):
-    def __init__(self):
+    def __init__(self, screenWidth=1920, screenHeight=1080,
+                 backgroundTargetWidth=None, backgroundTargetHeight=None,
+                 logoTargetWidth=750, logoTargetHeight=750):
         super().__init__()
+        self.screenWidth = screenWidth
+        self.screenHeight = screenHeight
 
+        assetsPath = "assets/"
+        self.backgroundTexturePath = assetsPath + "textures/background/red_brick_wall_1.png"
+        self.logoTexturePath = assetsPath + "textures/logo/icon.png"
+        defaultButtonTexturePath = assetsPath + "textures/gui/gui_button_1.png"
+        defaultButtonFontPath = assetsPath + "fonts/timer_and_counter_font.ttf"
 
-    def handleEvent(self, event):
+        rawBackgroundSurf = pg.image.load(self.backgroundTexturePath).convert()
+        targetBgWidth = backgroundTargetWidth if backgroundTargetWidth is not None else self.screenWidth
+        targetBgHeight = backgroundTargetHeight if backgroundTargetHeight is not None else self.screenHeight
+        self.backgroundSurf = pg.transform.scale(rawBackgroundSurf, (targetBgWidth, targetBgHeight))
+
+        self.logoSurf = pg.image.load(self.logoTexturePath).convert_alpha()
+        if logoTargetWidth is not None and logoTargetHeight is not None:
+            self.logoSurf = pg.transform.scale(self.logoSurf, (logoTargetWidth, logoTargetHeight))
+
+        self.logoRect = self.logoSurf.get_rect(
+            center=(self.screenWidth - 1350, self.screenHeight // 2))
+
+        firstButtonTopY = int(self.screenHeight - 1125)
+        buttonVerticalSpacing = -400
+
+        defaultButtonRotation = 0
+        defaultButtonTextColor = (250, 250, 250)
+        defaultButtonOutlineColor = (0, 0, 0)
+        defaultButtonOutlineThickness = 2
+        defaultButtonHoverColor = None
+
+        playButtonText = "play"
+        playButtonWidth = 850
+        playButtonHeight = 650
+        playButtonRotationAngle = 3
+        playButtonFontSize =70
+        playButtonTexture = defaultButtonTexturePath
+        playButtonFont = defaultButtonFontPath
+        playButtonTopLeftX = (self.screenWidth - 950)
+        playButtonTopLeftY = firstButtonTopY
+
+        self.playButton = Button(
+            x=playButtonTopLeftX, y=playButtonTopLeftY,
+            width=playButtonWidth, height=playButtonHeight,
+            texturePath=playButtonTexture,
+            text=playButtonText,
+            rotationAngle=playButtonRotationAngle,
+            fontPath=playButtonFont, fontSize=playButtonFontSize,
+            textColor=defaultButtonTextColor,
+            outlineColor=defaultButtonOutlineColor,
+            outlineThickness=defaultButtonOutlineThickness,
+            hoverEffectColor=defaultButtonHoverColor,
+            action=self.goToGameScene
+        )
+
+        settingsButtonText = "settings"
+        settingsButtonWidth = 850
+        settingsButtonHeight = 650
+        settingsButtonRotationAngle = -2
+        settingsButtonFontSize = 70
+        settingsButtonTexture = defaultButtonTexturePath
+        settingsButtonFont = defaultButtonFontPath
+        settingsButtonTopLeftX = (self.screenWidth - 950)
+        settingsButtonTopLeftY = playButtonTopLeftY + playButtonHeight + buttonVerticalSpacing
+
+        self.settingsButton = Button(
+            x=settingsButtonTopLeftX, y=settingsButtonTopLeftY,
+            width=settingsButtonWidth, height=settingsButtonHeight,
+            texturePath=settingsButtonTexture,
+            text=settingsButtonText,
+            rotationAngle=settingsButtonRotationAngle,
+            fontPath=settingsButtonFont, fontSize=settingsButtonFontSize,
+            textColor=defaultButtonTextColor,
+            outlineColor=defaultButtonOutlineColor,
+            outlineThickness=defaultButtonOutlineThickness,
+            hoverEffectColor=defaultButtonHoverColor,
+            action=self.goToSettingsScene
+        )
+
+        exitButtonText = "exit"
+        exitButtonWidth = 850
+        exitButtonHeight = 650
+        exitButtonRotationAngle = -1
+        exitButtonFontSize = 70
+        exitButtonTexture = defaultButtonTexturePath
+        exitButtonFont = defaultButtonFontPath
+        exitButtonTopLeftX = (self.screenWidth - 950)
+        exitButtonTopLeftY = settingsButtonTopLeftY + settingsButtonHeight + buttonVerticalSpacing
+
+        self.exitButton = Button(
+            x=exitButtonTopLeftX, y=exitButtonTopLeftY,
+            width=exitButtonWidth, height=exitButtonHeight,
+            texturePath=exitButtonTexture,
+            text=exitButtonText,
+            rotationAngle=exitButtonRotationAngle,
+            fontPath=exitButtonFont, fontSize=exitButtonFontSize,
+            textColor=defaultButtonTextColor,
+            outlineColor=defaultButtonOutlineColor,
+            outlineThickness=defaultButtonOutlineThickness,
+            hoverEffectColor=defaultButtonHoverColor,
+            action=self.quitGame
+        )
+
+        self.buttons = [self.playButton, self.settingsButton, self.exitButton]
+
+    def goToGameScene(self):
+        print("Przechodzenie do sceny gry (indeks 1)")
+        if self.sceneManager:
+            self.sceneManager.setCurrentScene(1)
+
+    def goToSettingsScene(self):
+        print("Przechodzenie do sceny ustawień (indeks 2)")
+        if self.sceneManager:
+            self.sceneManager.setCurrentScene(2)
+
+    def quitGame(self):
+        print("Wyjście z gry")
+        pg.quit()
+        sys.exit()
+
+    def handleEvent(self, event: pg.event.Event):
+        for button in self.buttons:
+            button.handleEvent(event)
+
         if event.type == pg.KEYDOWN:
-            if event.key == pg.K_2:
-                self.sceneManager.setCurrentScene(1)
+            if event.key == pg.K_ESCAPE:
+                self.quitGame()
 
-
-    def update(self, dt : float):
+    def update(self, dt: float):
         pass
 
+    def draw(self, screen: pg.Surface):
+        if self.backgroundSurf:
+            screen.blit(self.backgroundSurf, (0, 0))
+        else:
+            screen.fill((80, 30, 130))
 
-    def draw(self, screen : pg.Surface):
-        screen.fill("purple")
-        pass
+        if self.logoSurf and self.logoRect:
+            screen.blit(self.logoSurf, self.logoRect)
+
+        for button in self.buttons:
+            button.draw(screen)
