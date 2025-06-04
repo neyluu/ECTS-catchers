42aff5442fb874b0a0bec00a256ad3c026f43c8c
neyluu <75064225+neyluu@users.noreply.github.com>
2025-06-04 21:28:58 +0200

    Added running sound to Player

---
+++ src/game/LoopSound.py
@@ -0,0 +1,31 @@
+import pygame as pg
+
+class LoopSound:
+    def __init__(self, path : str, delay : float):
+        self.path = path
+        self.delay = delay
+
+        self.timer : delay = 0
+        self.playing : bool = False
+        self.sound = pg.mixer.Sound(path)
+
+        self.start()
+
+
+    def update(self, dt : float):
+        if not self.playing:
+            return
+
+        self.timer += dt
+        if self.timer >= self.delay:
+            print(self.timer)
+            self.sound.play()
+            self.timer -= self.delay
+
+
+    def start(self):
+        self.playing = True
+
+
+    def stop(self):
+        self.playing = False
