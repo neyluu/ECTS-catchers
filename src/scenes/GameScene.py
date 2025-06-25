ef30b27736dd01c7994e5f63b9c6b9f06d24e555
kacekhehee <kjanoska5@gmail.com>
2025-06-25 08:38:43 +0200

    Fixed backToMenuButton

--- src/scenes/GameScene.py
+++ src/scenes/GameScene.py
@@ -6,7 +6,7 @@ from src.config import Settings
 from src.gui.PauseMenu import PauseMenu
 from src.scenes.SettingsScene import SettingsScene
 from src.sounds.Soundtrack import Soundtrack
-
+from src.gui.Button import Button
 
 class GameScene(Scene):
     def __init__(self):
@@ -26,6 +26,21 @@ class GameScene(Scene):
         self.divider = pg.Rect(self.screenWidth // 2 - 2, 0, 4, self.screenHeight)
         self.pauseMenu = PauseMenu()
 
+        self.gameOverButton = Button(
+            x=Settings.SCREEN_WIDTH / 2 - 650 / 2,
+            y=750,
+            width=750, height=475,
+            texturePath="assets/textures/gui/gui_button_1.png",
+            text="main menu",
+            rotationAngle=0,
+            fontPath="assets/fonts/timer_and_counter_font.ttf",
+            fontSize=70,
+            textColor=(250, 250, 250),
+            outlineColor=(0, 0, 0),
+            outlineThickness=2,
+            hoverEffectColor=None,
+            action=self.goToMainMenu
+        )
     def handleEvent(self, event: pg.event.Event):
         if event.type == pg.KEYDOWN and event.key == Settings.KEYMAP_PAUSE:
             self.pauseMenu.toggle()
@@ -35,6 +50,8 @@ class GameScene(Scene):
         else:
             self.gameLeft.handleEvent(event)
             self.gameRight.handleEvent(event)
+        if self.bothGameOvers():
+            self.gameOverButton.handleEvent(event)
 
     def update(self, dt: float):
         if not self.running:
@@ -53,7 +70,8 @@ class GameScene(Scene):
         self.gameRight.draw(screen)
         pg.draw.rect(screen, (20, 20, 20), self.divider)
         self.pauseMenu.draw(screen)
-
+        if self.bothGameOvers():
+            self.gameOverButton.draw(screen)
     def reset(self):
         self.gameLeft.reset()
         self.gameRight.reset()
@@ -64,3 +82,17 @@ class GameScene(Scene):
             self.pauseMenu.isActive = True
         else:
             self.reset()
+
+    def goToMainMenu(self):
+        print("Going to main menu")
+        self.gameLeft.reset()
+        self.gameRight.reset()
+
+        print("Switching to Main Menu")
+        if self.sceneManager:
+            self.sceneManager.setCurrentScene(0)
+
+
+    def bothGameOvers(self) -> bool:
+        return self.gameLeft.isGameOver and self.gameRight.isGameOver \
+                and not self.gameLeft.gameOverAnimation.running and not self.gameRight.gameOverAnimation.running
